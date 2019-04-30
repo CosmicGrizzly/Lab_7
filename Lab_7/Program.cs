5e4a016258eb1b352f5c1544d09e53d6e5e12ce3
Jake Balch <InariOrchid@gmail.com>
2019-04-29 22:44:38 -0400

    Lab_7

---
+++ Lab_7/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables to store values
+            string name;
+            string email;
+            string phone;
+            string date;
+            bool validName;
+            bool validEmail;
+            bool validPhone;
+            bool validDate;
+            
+            Console.WriteLine("Please enter a valid Name: ");
+            name = Console.ReadLine();
+
+
+            //store true/false here so it doesn't get lost in space
+            //call your function/equation
+            validName = ValidateRegexString.ValidName(name);
+
+            //check if it's true or false
+
+            if (validName)
+            {
+                Console.WriteLine(" Name is Valid! ");
+            }
+            else
+            {
+                Console.WriteLine("The Name entered is invalid! ");
+            }
+            
+            //email
+            Console.WriteLine("Please enter a valid email: ");
+            email = Console.ReadLine();
+            validEmail = ValidateRegexString.ValidEmail(email);
+
+            if (validEmail)
+            {
+                Console.WriteLine(" email is Valid! ");
+            }
+            else
+            {
+                Console.WriteLine("The email entered is invalid! ");
+            }
+
+            //phone
+            Console.WriteLine("Please enter a valid phone number: ");
+            phone = Console.ReadLine();
+            validPhone = ValidateRegexString.ValidPhone(phone);
+
+            if (validPhone)
+            {
+                Console.WriteLine(" the phone number is Valid! ");
+            }
+            else
+            {
+                Console.WriteLine("The phone number entered is invalid! ");
+            }
+
+            //date
+            Console.WriteLine("Please enter a valid date: ");
+            date = Console.ReadLine();
+            validDate = ValidateRegexString.ValidPhone(date);
+
+            if (validDate)
+            {
+                Console.WriteLine(" date is Valid! ");
+            }
+            else
+            {
+                Console.WriteLine("The date entered is invalid! ");
+            }
+
+        }
+    }
+}
---
+++ Lab_7/ValidateRegexString.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lab_7
+{
+    class ValidateRegexString
+    {
+       public static bool ValidName(string y)
+        {
+            bool x = Regex.IsMatch(y, @"[A-Z][a-z]{1,29}");
+            return x;
+        }
+        public static bool ValidEmail(string email)
+        {
+            bool v = Regex.IsMatch(email, @"[a-zA-Z0-9]{5,30}@[a-zA-Z0-9]{5,10}\.[a-zA-Z0-9]{2,3}");
+            return v;
+        }
+        public static bool ValidPhone(string phone)
+        {
+            bool c = Regex.IsMatch(phone, @"[0-9]{1,3}[0-9]{1,3}[0-9]{1,4}");
+            return c;
+        }
+        public static bool ValidDate(string date)
+        {
+            bool regexDate = Regex.IsMatch(date, @"(([0-3]{1})([0-9]{1})(/)([0-1]{1})([0-9]{1})(/)([0-9]){4})");
+            return regexDate;
+        }
+    }
+}
